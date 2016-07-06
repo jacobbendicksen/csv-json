f5279f69ce9605fe47c5cd5e4f11cfe865b3cfa1
Jacob Bendicksen <jacob@jacobbendicksen.com>
2016-07-06 10:12:37 -0700

    beautified code

--- index.js
+++ index.js
@@ -18,75 +18,73 @@ var whichInterface = "web";
 var converter = new Converter({});
 prompt.start();
 
-converter.transform=function(json,row,index){
-	switch(source){
-		case "cellartracker":
-			json.color = json.Color;
-		    json.price = json.Price;
-		    json.name = json.Wine;
-		    json.size = json.Size;
-		    json.vintage = json.Vintage;
-		    json.varietal = json.Varietal;
-		    json.bottles = json.Quantity;
-		    json.region = json.Region;
-		    json.winery = json.Vineyard;
-		    json.drinkBy = json.EndConsume;
+converter.transform = function(json, row, index) {
+    switch (source) {
+        case "cellartracker":
+            json.color = json.Color;
+            json.price = json.Price;
+            json.name = json.Wine;
+            json.size = json.Size;
+            json.vintage = json.Vintage;
+            json.varietal = json.Varietal;
+            json.bottles = json.Quantity;
+            json.region = json.Region;
+            json.winery = json.Vineyard;
+            json.drinkBy = json.EndConsume;
 
-		    json.abv = "";
-		    json.img_path = "";
-		    json.external_img_path = "";
-		    json.ratingValue = "";
-		    json.ratingAuthor = "";
-		    json.ratingReview = "";
-		    json.ready = false;
-		    json.special = false;
-		    json.recentlyAdded = false;
-		    json.foodPairing = "";
+            json.abv = "";
+            json.img_path = "";
+            json.external_img_path = "";
+            json.ratingValue = "";
+            json.ratingAuthor = "";
+            json.ratingReview = "";
+            json.ready = false;
+            json.special = false;
+            json.recentlyAdded = false;
+            json.foodPairing = "";
 
-		    delete json.WS;
-		    delete json.WSWeb;
-		    delete json.Color;
-		    delete json.Price;
-		    delete json.Wine;
-		    delete json.Size;
-		    delete json.Vintage;
-		    delete json.Varietal;
-		    delete json.Quantity;
-		 	delete json.Region;
-		 	delete json.Vineyard;
-		 	delete json.EndConsume;
-		 	break;
-	}
+            delete json.WS;
+            delete json.WSWeb;
+            delete json.Color;
+            delete json.Price;
+            delete json.Wine;
+            delete json.Size;
+            delete json.Vintage;
+            delete json.Varietal;
+            delete json.Quantity;
+            delete json.Region;
+            delete json.Vineyard;
+            delete json.EndConsume;
+            break;
+    }
 };
 
-switch(whichInterface){
-	case "cli":
-		prompt.get(['filename', 'source'], function (err, result) {
-			source = result.source;
-			file = result.filename;
+switch (whichInterface) {
+    case "cli":
+        prompt.get(['filename', 'source'], function(err, result) {
+            source = result.source;
+            file = result.filename;
 
-	   		converter.on("end_parsed", function (jsonArray) {
-	   			console.log(jsonArray);
-			});
-			require("fs").createReadStream(file).pipe(converter);
- 		});
- 		break;
- 	case "web":
- 		app.get('/',function(req,res){
-			res.sendFile(path.join(__dirname+'/index.html'));		
-		});
+            converter.on("end_parsed", function(jsonArray) {
+                console.log(jsonArray);
+            });
+            require("fs").createReadStream(file).pipe(converter);
+        });
+        break;
+    case "web":
+        app.get('/', function(req, res) {
+            res.sendFile(path.join(__dirname + '/index.html'));
+        });
 
-		var uploader = multer({
-			dest: __dirname + '../uploads/',
-		});
+        var uploader = multer({
+            dest: __dirname + '../uploads/',
+        });
 
-		router.post('/upload', uploader, function(req, res) {
-			console.log("uploading");
-		});
+        router.post('/upload', function(req, res) {
+            console.log("uploading");
+        });
 
-		app.listen(3000);
- 		// file = /uploads/
-
-
- 		break;
+        app.listen(3000);
+        // file = /uploads/
+        break;
 }
